337488a6c948aa13062783476e3335d4b758cc4b
Berfin Tek <69851179+berfin-t@users.noreply.github.com>
2025-06-28 23:46:23 +0300

    Create Home page

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ my-portfolio/src/components/Home/Home.jsx
@@ -0,0 +1,48 @@
+﻿import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "./Particle";
+//import Type from "./Type";
+import HomeAbout from "./HomeAbout";
+import homeLogo from "../../assets/img/home-main.png";
+
+function Home() {
+    return (
+        <section>
+            <Container fluid className="home-section" id="home">
+                <Particle />
+                <Container className="home-content">
+                    <Row>
+                        <Col md={7} className="home-header">
+                            {/*<h1 style={{ paddingBottom: 15 }} className="heading">*/}
+                            {/*    Hi There!{" "}*/}
+                            {/*    <span className="wave" role="img" aria-labelledby="wave">*/}
+                                 
+                            {/*    </span>*/}
+                            {/*</h1>*/}
+                            <h1 className="heading-name">
+                                Merhaba Ben
+                                <strong className="main-name"> Berfin Tek</strong> 👋🏻
+                            </h1>
+                            <div style={{ padding: 50, textAlign: "left" }}>
+                            {/*    <Type />*/}
+                            </div>
+                        </Col>
+
+                        <Col md={5} style={{ paddingBottom: 20 }}>
+                            <img
+                                src={homeLogo}
+                                alt="home pic"
+                                className="img-fluid"
+                                style={{ maxHeight: "450px" }}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+            <HomeAbout/>
+        </section>
+    );
+
+}
+
+export default Home;
